634fa31ca40fb53688d28042960cc4299707176a
unknown <sahil.jamwal78625@gmail.com>
2019-06-18 00:51:24 +0530

    cpp files commit

---
+++ C++/Classes and Objects.cpp
@@ -0,0 +1,30 @@
+
+
+// Write your Student class here
+
+class Student{
+    int scores[5];
+public:
+    void input();
+    int calculateTotalScore();
+};
+
+void Student::input(){
+    for(int i=0;i<5;i++)
+    {
+    cin>>scores[i];
+    }
+}
+
+int Student::calculateTotalScore(){
+
+    int sum=0;
+    for(int i=0;i<5;i++){
+
+        sum+=scores[i];
+    }
+
+    return sum;
+
+}
+
