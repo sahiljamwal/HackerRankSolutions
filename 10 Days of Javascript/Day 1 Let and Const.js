19251538dc0f5b642a06c2c66832f92625ce7a19
unknown <sahil.jamwal78625@gmail.com>
2019-09-22 10:04:32 +0530

    commit

---
+++ 10 Days of Javascript/Day 1 Let and Const.js
@@ -0,0 +1,35 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+
+    const PI=Math.PI;
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r=readLine();
+    
+    // Print the area of the circle:
+    console.log(PI*r*r);
+    // Print the perimeter of the circle:
+    console.log(PI*r*2);
+    try {    
