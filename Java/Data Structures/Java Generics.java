ee87cf7346f1a5c2c2d8d9d55daedf78a711fee0
Sahil Jamwal <34044226+sahiljamwal@users.noreply.github.com>
2020-08-05 14:18:12 +0530

    Create Java Generics.java

---
+++ Java/Data Structures/Java Generics.java
@@ -0,0 +1,11 @@
+class Printer<t>
+{
+   //Write your code here
+   public  void printArray(t[] arr)
+   {
+       for(int i=0;i<arr.length;i++)
+       {
+           System.out.println(arr[i]);
+       }
+   }
+}
