825dbad1f607155c921681d68fc365217ac9dcde
Sahil Jamwal <34044226+sahiljamwal@users.noreply.github.com>
2020-08-05 13:07:21 +0530

    Delete Weather Observation Station 5.sql

--- Sql/Basic Select/Weather Observation Station 5.sql/* Enter your query here. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error. */Weather Observation Station 5.sql
+++
@@ -1,8 +0,0 @@
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
-
-SELECT  CITY,LENGTH(CITY) FROM (SELECT CITY,LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY),CITY) WHERE ROWNUM=1
-UNION
-SELECT  CITY,LENGTH(CITY) FROM (SELECT CITY,LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC,CITY DESC) WHERE ROWNUM=1;
