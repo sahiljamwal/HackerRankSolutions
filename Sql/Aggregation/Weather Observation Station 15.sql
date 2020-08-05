02dd4a4a903748f307e176027cd513db8766ae24
Sahil Jamwal <34044226+sahiljamwal@users.noreply.github.com>
2020-08-05 13:17:34 +0530

    Create Weather Observation Station 15.sql

---
+++ Sql/Aggregation/Weather Observation Station 15.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select round(long_w,4)
+from station
+where lat_n=(select max(lat_n) from station where lat_n<137.2345);
